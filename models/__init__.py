d4984ba2ecec28b1ed2b8ef2f0f42ae6bd50049b
James Burke <james.burke@eon-offgrid.com>
2018-04-16 20:35:40 +0200

    remove builtins for models

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,6 @@
-import builtins
 from sqlalchemy.ext.declarative import declarative_base
 
-import models
-builtins.models = models
-
-try:
-    Base = builtins.base
-except AttributeError:
-	Base = declarative_base()
+Base = declarative_base()
 
 from models.candidate import Candidate
 from models.employee import Employee
