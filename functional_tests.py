5baee770444a56092e239ba525402fdf3ee1b73a
James Burke <james.burke@eon-offgrid.com>
2018-04-17 18:26:01 +0200

    initial func test for getting job availability

--- functional_tests.py
+++ functional_tests.py
@@ -35,46 +35,48 @@ class FunctionalTestCase(unittest.TestCase):
 
     def test_create_employee_create_candidate_compare_availability(self):
 
-        ## create candidate
-        #with self.subTest(name="post successful request"):
+        # create candidate
         with boddle(method='POST', json={"name": "test1"}):
             self.assertEqual(candidate.post_candidate(db=self.db),
                 '{"id": 1, "name": "test1"}')
-
+        # get that candidate
         with boddle(method='GET'):
             candidate_record = json.loads(candidate.index(db=self.db))[0]
 
 
         ## create employee
-        #with self.subTest(name="post successful request"):
         with boddle(method='POST', json={"name": "test1", "title": "test1"}):
             self.assertEqual(employee.post_employee(db=self.db),
                 '{"id": 1, "name": "test1", "title": "test1"}')
-
+        # get that employee
         with boddle(method='GET'):
             employee_record = json.loads(employee.index(db=self.db))[0]
 
 
         ## setup job
-        #with self.subTest(name="post successful request"):
         with boddle(method='POST', json={"name": "test2"}):
             self.assertEqual(job.post_job(db=self.db),
                 '{"id": 1, "name": "test2"}')
-
+        # get that job
         with boddle(method='GET'):
             job_record = json.loads(job.index(db=self.db))[0]
 
         ## add availability for candidate
-        with self.subTest(name="post successful availability request"):
-            with boddle(method='POST', json={"job_id": job_record['id'], "candidate_id": candidate_record['id'], "availability": {'mon': [9,16]}}):
-                self.assertEqual(candidate.post_candidate_availability(db=self.db),
-                    '{"job_id": 1, "candidate_id": 1, "availability": "{\\"mon\\": [9, 16]}"}')
+        with boddle(method='POST', json={"job_id": job_record['id'], "candidate_id": candidate_record['id'], "availability": {'mon': [9,16]}}):
+            self.assertEqual(candidate.post_candidate_availability(db=self.db),
+                '{"job_id": 1, "candidate_id": 1, "availability": "{\\"mon\\": [9, 16]}"}')
 
         ## add availability for employee
-        with self.subTest(name="post successful availability request"):
-            with boddle(method='POST', json={"job_id": job_record['id'], "employee_id": employee_record['id'], "availability": {'mon': [9,16]}}):
-                self.assertEqual(employee.post_employee_availability(db=self.db),
-                    '{"job_id": 1, "employee_id": 1, "availability": "{\\"mon\\": [9, 16]}"}')
+        with boddle(method='POST', json={"job_id": job_record['id'], "employee_id": employee_record['id'], "availability": {'mon': [9,16]}}):
+            self.assertEqual(employee.post_employee_availability(db=self.db),
+                '{"job_id": 1, "employee_id": 1, "availability": "{\\"mon\\": [9, 16]}"}')
+
+
+        ## add get match of employee and candidate avialability times
+        with self.subTest(name="get availability of interview participants"):
+            with boddle(method='GET'):
+                self.assertEqual(job.get_attendee_availability(db=self.db),
+                    '{"availability": "{\\"mon\\": [9, 16]}"}')
 
 
 
